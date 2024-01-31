ddc39affa0575e2ca99579bb0df696ba8748bc48
obm <obm@AKASECs-iMac-2.local>
2024-01-31 04:27:14 +0100

    adding user checks

--- src/requirements/wordpress/tools/wp.sh
+++ src/requirements/wordpress/tools/wp.sh
@@ -6,15 +6,24 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 
 chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
 
-wp cli update --allow-root
+if ! wp core is-installed --allow-root; then
+    wp core download --allow-root
+    wp config create --dbname=wordpress --dbuser=obmium --dbpass=obmiumpass --dbhost=mariadb --allow-root
+    wp core install --url=localhost --title=wp --admin_user=obm --admin_password=obmpass --admin_email=obm@gmail.com --allow-root
+fi
 
-wp core download --allow-root
+python3 -m venv env
+source env/bin/activate > /dev/null 2>&1
 
-wp config create --dbname=wordpress --dbuser=obmium --dbpass=obmiumpass --dbhost=mariadb --allow-root
+if ! pip3 list | grep mysql-connector-python; then
+    pip3 install mysql-connector-python > /dev/null 2>&1
+fi
 
-wp core install --url=localhost --title=wp --admin_user=obm --admin_password=obmpass --admin_email=obm@gmail.com --allow-root
+user=$(python3 /bin/user_check.py walid) && if [ "$user" = "does not exist" ]; then
+    wp user create walid walid@gmail.com --role=author --user_pass=walidpass --allow-root
+fi > /dev/null 2>&1
 
-wp user create walid walid@gmail.com --role=author --user_pass=walidpass --allow-root
+deactivate > /dev/null 2>&1
 
 sed -i -e "s|/run/php/php8.2-fpm.sock|0.0.0.0:9000|g" "/etc/php/8.2/fpm/pool.d/www.conf"
 
