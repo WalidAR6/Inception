754f01c40fb28fe0201d909f782e8c8ec1cf6677
Walid Araissi <waraissi@e2r2p15.1337.ma>
2024-02-01 18:38:11 +0100

    add env variables

--- src/requirements/mariadb/tools/maria.sh
+++ src/requirements/mariadb/tools/maria.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
 
-export DATABASE="wordpress"
-export USERNAME="obmium"
-export USERPASS="obmiumpass"
-export ROOTPASS="rootpass"
-
 set -x
 
 sed -i -e "s/127.0.0.1/0.0.0.0/g" "/etc/mysql/mariadb.conf.d/50-server.cnf"
 
 service mariadb start
 
-sleep 5
-
 mysql_secure_installation << EOF > /dev/null 2>&1
 
 n
 y
-$ROOTPASS
-$ROOTPASS
+$MARIADB_ROOT_PASSWORD
+$MARIADB_ROOT_PASSWORD
 y
 n
 n
 y
 EOF
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS $DATABASE;"
+mariadb -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;"
 
-mariadb -e "CREATE USER IF NOT EXISTS '$USERNAME'@'%' IDENTIFIED BY '$USERPASS';"
+mariadb -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$USERNAME'@'%' WITH GRANT OPTION;"
+mariadb -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' WITH GRANT OPTION;"
 
 mariadb -e "FLUSH PRIVILEGES;"
 
 service mariadb stop
 
-mysqld --user=root
+mariadbd --user=root
