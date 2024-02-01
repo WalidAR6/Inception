b13e78dbf4b74085e929530367b0b217461a973f
Walid Araissi <waraissi@e2r2p15.1337.ma>
2024-02-01 18:39:18 +0100

    remove index.html from configfile, and add the ssl cert files in env

--- src/requirements/nginx/tools/ssl-req.sh
+++ src/requirements/nginx/tools/ssl-req.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfs.key -out /etc/ssl/certs/nginx-selfs.crt << EOF > /dev/null 2>&1
+openssl req -x509 -nodes -newkey rsa:2048 -keyout $CERT_KEY -out $CERT << EOF > /dev/null 2>&1
 CA
 QC
 Company Inc
