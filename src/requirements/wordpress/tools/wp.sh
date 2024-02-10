65d9d9c130c7a5d1ae91fc9762b523fe830643f5
Walid Araissi <waraissi@e3r10p11.1337.ma>
2024-02-10 20:17:30 +0100

    install redis plugin and changing debian version

--- src/requirements/wordpress/tools/wp.sh
+++ src/requirements/wordpress/tools/wp.sh
@@ -14,22 +14,20 @@ if ! wp core is-installed --allow-root; then
     wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
 fi
 
-python3 -m venv env
-source env/bin/activate > /dev/null 2>&1
+wp user create $WP_AUTHOR $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PASSWORD --allow-root
 
-if ! pip3 list | grep mysql-connector-python; then
-    pip3 install mysql-connector-python > /dev/null 2>&1
-fi
+sed -i -e "s|/run/php/php7.4-fpm.sock|0.0.0.0:9000|g" "/etc/php/7.4/fpm/pool.d/www.conf"
 
-user=$(python3 /bin/user_check.py $WP_AUTHOR) && if [ "$user" = "does not exist" ]; then
-    wp user create $WP_AUTHOR $WP_AUTHOR_EMAIL --role=author --user_pass=$WP_AUTHOR_PASSWORD --allow-root
-fi > /dev/null 2>&1
+wp config set WP_REDIS_PORT 6379 --allow-root
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set FS_METHOD direct --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp plugin install health-endpoint --activate --allow-root
+chown www-data:www-data *
 
-deactivate > /dev/null 2>&1
+wp redis enable --allow-root
 
-sed -i -e "s|/run/php/php8.2-fpm.sock|0.0.0.0:9000|g" "/etc/php/8.2/fpm/pool.d/www.conf"
+service php7.4-fpm start
+service php7.4-fpm stop
 
-service php8.2-fpm start
-service php8.2-fpm stop
-
-php-fpm8.2 -F
+php-fpm7.4 -F
