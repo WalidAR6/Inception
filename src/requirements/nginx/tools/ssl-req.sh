b7e38dd2645a70f3104855196bea56878d880835
walid <araissiwalid15@gmail.com>
2024-02-24 05:19:42 -0500

    add envsubst to exapand env variables in conf file, removing set x

--- src/requirements/nginx/tools/ssl-req.sh
+++ src/requirements/nginx/tools/ssl-req.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 openssl req -x509 -nodes -newkey rsa:2048 -keyout $CERT_KEY -out $CERT << EOF > /dev/null 2>&1
 CA
 QC
@@ -12,5 +10,4 @@ example.com
 ljnrvlss
 
 EOF
-
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/sites-available/default && nginx -g "daemon off;"
+envsubst '${DOMAIN_NAME} ${CERT} ${CERT_KEY}' < /etc/nginx/nginx.conf.template > /etc/nginx/sites-available/default && nginx -g "daemon off;"
